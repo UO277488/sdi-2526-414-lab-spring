3919805a8dda965abc61455af5a55640f65e0892
uo277488 <uo277488@uniovi.es>
2026-02-10 20:53:39 +0100

    SDI-2526-414-2.1-Creating Spring project.

---
+++ GradeManager/src/main/java/com/uniovi/sdi/grademanager/GradeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.grademanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeManagerApplication.class, args);
+    }
+
+}
---
+++ GradeManager/src/test/java/com/uniovi/sdi/grademanager/GradeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.grademanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
